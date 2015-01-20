f1d08a9f00d50947ad323bd42dae8c729256fc1b
EMILY FAN <emilyjustinefan@gmail.com>
2015-01-20 16:18:57 -0500

    subtracting 1 year from date

--- exercise3.rb
+++ exercise3.rb
@@ -10,6 +10,6 @@ puts "You have really great skin! Do you mind telling me your age?"
 
 age = gets.chomp
 
-birth_year = Date.today.year - age.to_i
+birth_year = (Date.today.year - age.to_i) - 1
 
 puts "Wow! I would have never guessed that you were born in #{birth_year}!"
