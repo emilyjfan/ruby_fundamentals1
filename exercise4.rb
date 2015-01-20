65dad46595c045f000496cda482aa716a1db1c10
EMILY FAN <emilyjustinefan@gmail.com>
2015-01-20 15:18:19 -0500

    initial commit for Exercise 4

---
+++ exercise4.rb
@@ -0,0 +1,26 @@
+(1..100).each do |x|
+	if x % 3 == 0 && x % 5 == 0 
+		puts "BitMaker"
+	elsif x % 3 == 0 
+		puts "Bit"
+	elsif x % 5 == 0 
+		puts "Maker"
+	elsif 
+		puts x 
+	end
+end
+
+#using for 
+using for... 
+for num in 1..100
+		if num % 3 == 0 && num % 5 == 0 
+			print "BitMaker "
+		elsif num % 3 == 0	
+			print "Bit "
+		elsif num % 5 == 0
+			print "Maker "
+		else 
+			print "#{num} " 
+		end 	
+end 
+
