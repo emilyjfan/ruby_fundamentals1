85c72da39a61818fe1f1dbdf703089223d179137
EMILY FAN <emilyjustinefan@gmail.com>
2015-01-20 14:46:14 -0500

    Added question about age and string with birth year

--- exercise3.rb
+++ exercise3.rb
@@ -1,6 +1,15 @@
+require 'date'
+
 puts "What is your name?"
 
 name = gets.chomp
 
 puts "Hi #{name}!"
 
+puts "You have really great skin! Do you mind telling me your age?"
+
+age = gets.chomp
+
+birth_year = Date.today.year - age.to_i
+
+puts "Wow! I would have never guessed that you were born in #{birth_year}!"
