b2f6c9ebaa21689e4ea950063aa83171674f76d7
EMILY FAN <emilyjustinefan@gmail.com>
2015-01-20 14:28:22 -0500

    Exercise 2 additions

--- exercise2.rb
+++ exercise2.rb
@@ -1,3 +1,18 @@
+#How to calculate the tip amount for a meal
 meal = 55
 tip = meal * 20/100 
 puts "The tip for a $#{meal} equals $#{tip}"
+
+#Adding a string an an integer 
+puts "10".to_i + 10 
+
+#45628 multiplied by 7839 in a sentence
+num1 = 45628
+num2 = 7839 
+result = num1 * num2
+
+puts "The result of #{num1} times #{num2} equals #{result}."
+
+#Boolean expression 
+
+puts (true && false) || (false && true) || !(false && false) 
